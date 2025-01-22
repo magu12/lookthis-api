d122bce02240f1a21337db73aba70d7072d6d200
magerram gubadov <magerram.gubadov@niwart.io>
2025-01-22 14:19:36 +0300

    Remove api keys ;)

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dxpusp7zw',
-  api_key: '375814694341387',
-  api_secret: 'Nqs80jLmIM5ebExfMgY7WSioCLs'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary; 
